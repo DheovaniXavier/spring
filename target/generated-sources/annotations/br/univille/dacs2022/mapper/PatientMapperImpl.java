6336f02cba4cab704e277036a1e4faf9d524f542
Dheovani <dheovani_xavier@outlook.com>
2022-07-06 19:55:13 -0300

    Gogogo

--- target/generated-sources/annotations/br/univille/dacs2022/mapper/PatientMapperImpl.java
+++ target/generated-sources/annotations/br/univille/dacs2022/mapper/PatientMapperImpl.java
@@ -8,8 +8,8 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-06-22T19:26:35-0300",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.15 (Private Build)"
+    date = "2022-07-06T19:54:19-0300",
+    comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.3 (Eclipse Adoptium)"
 )
 public class PatientMapperImpl implements PatientMapper {
 
@@ -49,10 +49,10 @@ public class PatientMapperImpl implements PatientMapper {
 
         PatientDTO patientDTO = new PatientDTO();
 
+        patientDTO.setBirthDate( patient.getBirthDate() );
         patientDTO.setId( patient.getId() );
         patientDTO.setName( patient.getName() );
         patientDTO.setSex( patient.getSex() );
-        patientDTO.setBirthDate( patient.getBirthDate() );
 
         return patientDTO;
     }
@@ -65,10 +65,10 @@ public class PatientMapperImpl implements PatientMapper {
 
         Patient patient1 = new Patient();
 
+        patient1.setBirthDate( patient.getBirthDate() );
         patient1.setId( patient.getId() );
         patient1.setName( patient.getName() );
         patient1.setSex( patient.getSex() );
-        patient1.setBirthDate( patient.getBirthDate() );
 
         return patient1;
     }
